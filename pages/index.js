a7e162c1a4e006af16d45584eee5b2a4acce24b4
SlytherinScriptor <159412730+SlytherinScriptor@users.noreply.github.com>
2026-02-26 15:35:50 +0530

    [Agent] Create: pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WebApp</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Welcome to WebApp</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
